2c1c103088cbf14c9b31757e1369569b38029cb9
CieranSol <dungeon.master.cieran@gmail.com>
2021-12-20 18:58:27 -1000

    Update listchannels.js

--- utilities/listchannels.js
+++ utilities/listchannels.js
@@ -1,8 +1,7 @@
 // for running scheduled tasks such as leaderboard awards
 import { Client, Intents } from "discord.js";
 import moment from "moment-timezone";
-import config from "../config.js";
-import { GUILD_ID } from "../config.js";
+import { GUILD_ID, BOT_TOKEN } from "../config.js";
 
 // Tell moment, our date library, that Monday is the first day of the week.
 moment.updateLocale("en", {
@@ -22,9 +21,35 @@ const client = new Client({
     ],
 });
 
-client.login(config.BOT_TOKEN).then(async () => {
+client.login(BOT_TOKEN).then(async () => {
     const guild = await client.guilds.fetch(GUILD_ID);
     const channels = await guild.channels.fetch();
-    const channelarray = channels.map((c) => c.name);
-    console.log(channelarray.join(" "), channelarray.length);
+    // const channelarray = channels.map((c) => c.name);
+    // console.log(channelarray.join(" "), channelarray.length);
+    channels.forEach(async (c) => {
+        // const ch = await client.channels.cache.get(c.id);
+        // const m = ch.messages;
+        // console.log(c.type, c.lastMessageId);
+        if (c.type === "GUILD_TEXT") {
+            if (c.lastMessageId) {
+                try {
+                    const message = await c.messages.fetch(c.lastMessageId);
+                    const date = moment(message.createdTimestamp);
+                    const isInactive = moment().diff(date, "months") >= 1;
+                    if (isInactive) {
+                        console.log(
+                            c.name,
+                            moment(message.createdTimestamp).format()
+                        );
+                    }
+                } catch (e) {
+                    console.log(c.name, "error");
+                }
+            } else {
+                console.log(c.name, "no messages");
+            }
+        }
+        await new Promise((r) => setTimeout(r, 1000));
+        // console.log(`${c.name}: ${c}`);
+    });
 });
